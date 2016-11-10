514f025dd8d20b4aa7445601272bb47a38719209
Chris Woods <chris@chriswoods.me.uk>
2016-11-10 20:14:06 +0000

    first

---
+++ src/main/java/co/uk/popupengineering/SpringBootCamelActivemqApplication.java
@@ -0,0 +1,12 @@
+package co.uk.popupengineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCamelActivemqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCamelActivemqApplication.class, args);
+	}
+}
